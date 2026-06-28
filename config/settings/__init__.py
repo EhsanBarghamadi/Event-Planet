c457d04e55fe5970ad9836bf58d57c64e023dbc9
ehsan barghamadi <ehsan0barghamadi@gmail.com>
2026-06-28 14:31:41 +0330

    feat(auth): add custom permission classes for event management

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-env = os.getenv('DJNAGO_ENV', 'dev').lower
+env = os.getenv('DJNAGO_ENV', 'dev').lower()
 
 if env == 'prod':
     from .prod import *
---
+++ core/permissions.py
@@ -0,0 +1,24 @@
+from rest_framework import permissions
+
+class IsOrganizer(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user and request.user.is_authenticated and request.user.role == 'ORGANIZER':
+            return True
+        return False
+    
+class IsParticipant(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user and request.user.is_authenticated and request.user.role == 'PARTICIPANT':
+            return True
+        return False
+        
+    
+class IsEventOwner(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        event = obj if hasattr(obj , 'organizer') else getattr(obj, 'event', None)
+        return bool(event and event.organizer_id == request.user.id)
