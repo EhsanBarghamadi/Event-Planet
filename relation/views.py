02f324f09e706cb8b17d9c3cbdf7c5a6a8120105
ehsan barghamadi <ehsan0barghamadi@gmail.com>
2026-06-29 18:49:46 +0330

    feat(views): add RegistrationViewSet for event registration management

--- relation/views.py
+++ relation/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from core.permissions import IsParticipant
+from .models import Registration
+from .serializers import RegistrationSerializer
 
-# Create your views here.
+class RegistrationViewSet(viewsets.ModelViewSet):
+    serializer_class = RegistrationSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [permissions.IsAuthenticated(), IsParticipant()]
+        return [permissions.IsAuthenticated()]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Registration.objects.select_related('event').filter(participant=user)
+
+    def perform_create(self, serializer):
+        serializer.save(participant=self.request.user)
