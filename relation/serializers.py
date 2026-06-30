d1b2ded76bd3651ae42ec4691311d7f99d8bfbde
ehsan barghamadi <ehsan0barghamadi@gmail.com>
2026-06-30 17:30:21 +0330

    feat(serializers): add FeedbackSerializer with validation rules

--- relation/serializers.py
+++ relation/serializers.py
@@ -34,3 +34,39 @@ class RegistrationSerializer(serializers.ModelSerializer):
                 'participant':'شما قبلا ثبت نام کرده اید'
             })
         return attrs
+    
+class FeedbackSerializer(serializers.ModelSerializer):
+    event = EventSerializer(read_only=True)
+    event_id = serializers.PrimaryKeyRelatedField(
+        source='event',
+        queryset=Event.objects.all(),
+        write_only=True
+    )
+    class Meta:
+        model = Feedback
+        fields = ['participant', 'event', 'rating', 'comment']
+        read_only_fields = ['participant']
+
+    def validate(self, attrs):
+        event = attrs.get('event')
+        request = self.context.get('request')
+
+        if event.status != 'FINISHED':
+            raise serializers.ValidationError({
+                'event': 'فقط برای رویدادهای پایان‌یافته می‌توانید بازخورد ثبت کنید'
+            })
+        
+        registration = Registration.objects.filter(event=event, participant=request.user)
+        if not registration.exists():
+            raise serializers.ValidationError({
+                'participant': 'برای ثبت نظر باید در دوره مورد نظر ثبت نام کنید'
+            })
+        
+        feedback = Feedback.objects.filter(event=event, participant=request.user)
+        if feedback.exists():
+            raise serializers.ValidationError({
+                'participant': 'شما قبلا نظر خود را ثبت کرده اید'
+            })
+        
+
+        return attrs
