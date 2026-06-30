016a811e5cc5df33244cb3305894bf69da26757d
ehsan barghamadi <ehsan0barghamadi@gmail.com>
2026-06-30 17:54:45 +0330

    feat: add FeedbackViewSet with role-based filtering

--- relation/views.py
+++ relation/views.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 from core.permissions import IsParticipant
-from .models import Registration
-from .serializers import RegistrationSerializer
+from .models import Registration, Feedback
+from .serializers import RegistrationSerializer, FeedbackSerializer
 
 class RegistrationViewSet(viewsets.ModelViewSet):
     serializer_class = RegistrationSerializer
@@ -16,5 +16,24 @@ class RegistrationViewSet(viewsets.ModelViewSet):
         user = self.request.user
         return Registration.objects.select_related('event').filter(participant=user)
 
+    def perform_create(self, serializer):
+        serializer.save(participant=self.request.user)
+
+class FeedbackViewSet(viewsets.ModelViewSet):
+    serializer_class = FeedbackSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == 'PARTICIPANT':
+            return Feedback.objects.filter(participant=user)
+        if user.role == 'ORGANIZER' and user.events.exists():
+            return Feedback.objects.filter(event__organizer=user)
+        return Feedback.objects.none()
+    
+    def get_permissions(self):
+        if self.action == 'create':
+            return [permissions.IsAuthenticated(), IsParticipant()]
+        return [permissions.IsAuthenticated()]
+    
     def perform_create(self, serializer):
         serializer.save(participant=self.request.user)
