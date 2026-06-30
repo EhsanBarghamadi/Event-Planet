511e6fe8c556183350d2aa8d6824db4927f005ba
ehsan barghamadi <ehsan0barghamadi@gmail.com>
2026-06-30 18:00:16 +0330

    feat(urls): add eventstage and feedback API endpoints

--- event/urls.py
+++ event/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EventViewSet
+from .views import EventViewSet, EventStageViewSet
 
 router = DefaultRouter()
 
 router.register(r'event', EventViewSet, basename='event')
+router.register(r'eventstage', EventStageViewSet, basename='eventstage')
 
 urlpatterns = [
     path('', include(router.urls))
--- relation/urls.py
+++ relation/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegistrationViewSet
+from .views import RegistrationViewSet, FeedbackViewSet
 
 router = DefaultRouter()
 
 router.register(r'registration', RegistrationViewSet, basename='registration')
+router.register(r'feedback', FeedbackViewSet, basename='feedback')
 
 urlpatterns = [
     path('', include(router.urls))
