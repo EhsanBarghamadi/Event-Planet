7c5f9fbdb8390de172f90dd042e71dc492336346
ehsan barghamadi <ehsan0barghamadi@gmail.com>
2026-07-07 06:48:59 +0330

    feat(api): implement complete EventSerializer validation

--- event/serializers.py
+++ event/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from django.utils import timezone
 from attribute.serializers import EventAttributeValueSerializer
-from rest_framework.exceptions import PermissionDenied
+from rest_framework.exceptions import PermissionDenied, ValidationError
 from .models import Event, EventStage
 
 class EventSerializer(serializers.ModelSerializer):
     attribute_values = EventAttributeValueSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Event
         fields = ['id', 'organizer', 'title', 'description', 
@@ -15,10 +16,54 @@ class EventSerializer(serializers.ModelSerializer):
 
     def validate_start_date(self, value):
         today = timezone.localdate()
-        if value < today:
+        if value.date() < today:
             raise serializers.ValidationError('تاریخ شروع رویداد نمی‌تواند در گذشته باشد')
         return value
+    
+    def validate_capacity(self, value):
+        if value <= 0:
+            raise serializers.ValidationError('ظرفیت رویداد باید بزرگتر از صفر باشد.')
+        return value
+    
+    def validate(self, attrs):
+        if self.instance:
+            start_date = attrs.get('start_date', self.instance.start_date)
+            end_date = attrs.get('end_date', self.instance.end_date)
+        else:
+            start_date = attrs.get('start_date')
+            end_date = attrs.get('end_date')
+        new_status = attrs.get('status')
 
+        if start_date and end_date:
+            if end_date <= start_date:
+                raise serializers.ValidationError({
+                    'end_date': 'تاریخ پایان رویداد نمیتواند قبل یا همزمان با تاریخ شروع آن باشد'
+                })
+            
+        if not self.instance:
+            if new_status is None:
+                attrs['status'] = Event.Status.DRAFT
+            elif new_status != Event.Status.DRAFT:
+                raise serializers.ValidationError({
+                    'status': 'وضعیت رویداد در زمان اجرا فقط میتواند "پیش نویس" باشد'
+                })
+
+        if self.instance:
+            original_status = self.instance.status
+            
+            if new_status and original_status != new_status:
+                if not Event.is_transition_allowed(original_status, new_status):
+                    allowed_list = Event.get_allowed_transitions(original_status)
+                    allowed_statuses = [status.value for status in allowed_list] if allowed_list else ['هیچکدام']
+                    raise serializers.ValidationError({
+                        'status': (
+                            f'تغییر وضعیت از {original_status} به {new_status} مجاز نیست'
+                            f'وضعیت مجاز {", ".join(allowed_statuses)}'
+                        )
+                    })
+                
+        return attrs
+    
 class EventStageSerializer(serializers.ModelSerializer):
     event = EventSerializer(read_only=True)
     event_id = serializers.PrimaryKeyRelatedField(
@@ -91,4 +136,7 @@ class EventStageSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError(
                     'این مرحله با مراحل دیگر تداخل زمانی دارد'
                 )
+            
+            if instance:
+
         return attrs
