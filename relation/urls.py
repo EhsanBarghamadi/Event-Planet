4410401784c2eba85a52f67d18f9ebd791c2a404
ehsan barghamadi <ehsan0barghamadi@gmail.com>
2026-07-09 12:21:07 +0330

    feat(urls): add Result API endpoints

--- relation/urls.py
+++ relation/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegistrationViewSet, FeedbackViewSet
+from .views import RegistrationViewSet, FeedbackViewSet, ResultViewSet
 
 router = DefaultRouter()
 
 router.register(r'registration', RegistrationViewSet, basename='registration')
 router.register(r'feedback', FeedbackViewSet, basename='feedback')
+router.register(r'result', ResultViewSet, basename='result')
 
 urlpatterns = [
     path('', include(router.urls))
