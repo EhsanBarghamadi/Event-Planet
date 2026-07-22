83a85eb44bca027ffae3016c22fd8529a03cc583
Ehsan Barghamadi <ehsan0barghamadi@gmail.com>
2026-07-22 04:42:01 +0330

    fix(event): finalize EventAttributeValueSerializer with DRAFT status check and field-level error messages

--- attribute/serializers.py
+++ attribute/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from event.models import Event
 from .models import Attribute, EventAttributeValue
 
 class AttributeSerializer(serializers.ModelSerializer):
@@ -16,21 +18,34 @@ class EventAttributeValueSerializer(serializers.ModelSerializer):
         fields = ['id', 'event', 'attribute', 'value_text', 'value_int', 'value_bool', 'created_at', 'updated_at']
 
     def validate(self, attrs):
-        request = self.context.get('request')
         if self.instance:
             event = attrs.get('event', self.instance.event)
             attribute = attrs.get('attribute', self.instance.attribute)
+            value_text = attrs.get('value_text', self.instance.value_text)
+            value_int = attrs.get('value_int', self.instance.value_int)
+            value_bool = attrs.get('value_bool', self.instance.value_bool)
+
+            if 'event' in attrs and attrs['event'] != self.instance.event:
+                raise serializers.ValidationError({'event': 'تغییر رویداد مجاز نیست!'})
+            if 'attribute' in attrs and attrs['attribute'] != self.instance.attribute:
+                raise serializers.ValidationError({'attribute': 'تغییر ویژگی مجاز نیست!'})
+            
         else:
             event = attrs.get('event')
             attribute = attrs.get('attribute')
-        
-        if event.organizer != request.user:
-            raise serializers.ValidationError('شما مالک این رویداد نیستید!')
-        
-        if attribute:
             value_text = attrs.get('value_text')
             value_int = attrs.get('value_int')
             value_bool = attrs.get('value_bool')
+
+        if not event:
+            raise serializers.ValidationError({'event': 'انتخاب رویداد الزامی است!'})
+        
+        if event.status != Event.Status.DRAFT:
+            raise serializers.ValidationError({
+                'event': 'امکان ثبت ویژگی فقط روی رویداد های پیش نویس وجود دارد.'
+            })
+        
+        if attribute:
             if attribute.data_type == "TEXT":
                 if value_int is not None or value_bool is not None or value_text is None:
                     raise serializers.ValidationError('فقط باید فیلد متن پر شود.')
