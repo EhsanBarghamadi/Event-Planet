3f27345ed6f2c725de3eabe1dda94fce91b70d44
Ehsan Barghamadi <ehsan0barghamadi@gmail.com>
2026-08-04 20:55:11 +0330

    feat(attribute): add factories for Attribute and EventAttributeValue

---
+++ attribute/factories.py
@@ -0,0 +1,37 @@
+import factory
+from faker import Faker
+
+from .models import Attribute, EventAttributeValue
+
+fake = Faker()
+
+class AttributeFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = Attribute
+
+    class Params:
+        text = factory.Trait(data_type = Attribute.DataType.TEXT)
+        integer = factory.Trait(data_type = Attribute.DataType.INTEGER)
+        boolean = factory.Trait(data_type = Attribute.DataType.BOOLEAN)
+
+    name = factory.Sequence(lambda n: f'attribute-{n}')
+    data_type = Attribute.DataType.TEXT
+
+
+class EventAttributeValueFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = EventAttributeValue
+
+    event = factory.SubFactory('event.factories.EventFactory')
+    attribute = factory.SubFactory(AttributeFactory)
+    value_text = factory.LazyAttribute(
+        lambda obj: fake.sentence() if obj.attribute.data_type == Attribute.DataType.TEXT else None
+        )
+    value_int = factory.LazyAttribute(
+        lambda obj: fake.random_int() if obj.attribute.data_type == Attribute.DataType.INTEGER else None
+    )
+    value_bool = factory.LazyAttribute(
+        lambda obj: fake.boolean() if obj.attribute.data_type == Attribute.DataType.BOOLEAN else None
+    )
