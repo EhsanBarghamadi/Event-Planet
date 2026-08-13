84c1a159ca9aec3afe2d1404b8fc3a7b78010577
Ehsan Barghamadi <ehsan0barghamadi@gmail.com>
2026-08-13 20:18:30 +0330

    test(user): add JWT authentication tests

--- user/tests/test_auth_api.py
+++ user/tests/test_auth_api.py
@@ -69,6 +69,7 @@ def test_register_user_with_invalid_phone(api_client):
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert "phone" in response.data
 
+
 @pytest.mark.django_db
 def test_register_user_with_invalid_password(api_client):
     register_url = reverse("register", kwargs={"version": "v1"})
@@ -83,6 +84,7 @@ def test_register_user_with_invalid_password(api_client):
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert "password" in response.data
+
 
 @pytest.mark.django_db
 def test_register_user_with_repetitive_phone(api_client):
@@ -101,4 +103,49 @@ def test_register_user_with_repetitive_phone(api_client):
     assert "phone" in response.data
 
 
+@pytest.mark.django_db
+def test_login_user_with_jwt_success(api_client):
+    user = CustomUserFactory(phone="09000000000", password="StrongPass9!x")
+    login_url = reverse("token_obtain_pair", kwargs={"version": "v1"})
+    user_data = {"phone": user.phone, "password": "StrongPass9!x"}
 
+    response = api_client.post(login_url, data=user_data)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert "refresh" in response.data
+    assert "access" in response.data
+    assert "first_name" in response.data
+    assert "role" in response.data
+    assert "phone" in response.data
+    assert response.data["first_name"] == user.first_name
+    assert response.data["role"] == user.role
+    assert response.data["phone"] == user.phone
+
+
+@pytest.mark.django_db
+def test_login_user_jwt_with_wrong_password(api_client):
+    user = CustomUserFactory(phone="09000000000", password="StrongPass9!x")
+    login_url = reverse("token_obtain_pair", kwargs={"version": "v1"})
+    user_data = {"phone": user.phone, "password": "wrong_password"}
+
+    response = api_client.post(login_url, data=user_data)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.django_db
+def test_refresh_jwt_token_success(api_client):
+    user = CustomUserFactory(phone="09000000000", password="StrongPass9!x")
+    login_url = reverse("token_obtain_pair", kwargs={"version": "v1"})
+    refresh_url = reverse("token_refresh", kwargs={"version": "v1"})
+    user_data = {"phone": user.phone, "password": "StrongPass9!x"}
+
+    response_login = api_client.post(login_url, data=user_data)
+
+    assert response_login.status_code == status.HTTP_200_OK
+
+    data = {"refresh": response_login.data["refresh"]}
+    response_refresh = api_client.post(refresh_url, data=data)
+
+    assert response_refresh.status_code == status.HTTP_200_OK
+    assert "access" in response_refresh.data
