5fbb83e123527becf54096a46c8f45d53c59d81c
Ehsan Barghamadi <ehsan0barghamadi@gmail.com>
2026-08-24 21:12:51 +0330

    test(event): add event creation permission tests

--- event/tests/test_event_api.py
+++ event/tests/test_event_api.py
@@ -65,6 +65,7 @@ def test_organizer_can_see_published_events_and_own_drafts(get_auth_client):
     assert any(event["id"] == other_published.id for event in response.data)
     assert all(event["id"] != other_draft.id for event in response.data)
 
+
 @pytest.mark.django_db
 def test_organizer_can_create_event(get_auth_client):
     organizer = CustomUserFactory(organizer=True, password="StrongPass9!x")
@@ -79,8 +80,8 @@ def test_organizer_can_create_event(get_auth_client):
         "start_date": "2026-11-01T09:00:00Z",
         "title": "همایش تخصصی هوش مصنوعی و یادگیری ماشین",
         "description": "این همایش با هدف آشنایی با جدیدترین دستاوردهای حوزه هوش مصنوعی و کاربردهای آن در صنعت برگزار می‌شود. حضور برای عموم آزاد است.",
-        "status": "DRAFT"
-        }
+        "status": "DRAFT",
+    }
 
     client = get_auth_client(organizer, password="StrongPass9!x")
     response = client.post(event_url, data=event_data)
@@ -89,3 +90,44 @@ def test_organizer_can_create_event(get_auth_client):
     assert response.data["organizer"] == organizer.id
     assert response.data["status"] == "DRAFT"
     assert response.data["capacity"] == 150
+
+
+@pytest.mark.django_db
+def test_participant_cannot_create_event(get_auth_client):
+    participant = CustomUserFactory(participant=True, password="StrongPass9!x")
+
+    event_url = reverse("event-list", kwargs={"version": "v1"})
+
+    event_data = {
+        "organizer": participant.id,
+        "capacity": 150,
+        "end_date": "2026-12-20T18:00:00Z",
+        "start_date": "2026-11-01T09:00:00Z",
+        "title": "همایش تخصصی هوش مصنوعی و یادگیری ماشین",
+        "description": "این همایش با هدف آشنایی با جدیدترین دستاوردهای حوزه هوش مصنوعی و کاربردهای آن در صنعت برگزار می‌شود. حضور برای عموم آزاد است.",
+        "status": "DRAFT",
+    }
+
+    client = get_auth_client(participant, password="StrongPass9!x")
+    response = client.post(event_url, data=event_data)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert response.data["detail"].code == "permission_denied"
+
+@pytest.mark.django_db
+def test_guest_cannot_create_event(api_client):
+    event_url = reverse("event-list", kwargs={"version": "v1"})
+
+    event_data = {
+        "capacity": 150,
+        "end_date": "2026-12-20T18:00:00Z",
+        "start_date": "2026-11-01T09:00:00Z",
+        "title": "همایش تخصصی هوش مصنوعی و یادگیری ماشین",
+        "description": "این همایش با هدف آشنایی با جدیدترین دستاوردهای حوزه هوش مصنوعی و کاربردهای آن در صنعت برگزار می‌شود. حضور برای عموم آزاد است.",
+        "status": "DRAFT"
+        }
+
+    response = api_client.post(event_url, data=event_data)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.data["detail"].code == "not_authenticated"
