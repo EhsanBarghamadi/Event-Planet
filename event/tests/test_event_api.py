9dcbb2dfc96d875512c1b3956696869015868f8c
Ehsan Barghamadi <ehsan0barghamadi@gmail.com>
2026-09-05 21:38:33 +0330

    test(event): add event update and state transition tests

--- event/tests/test_event_api.py
+++ event/tests/test_event_api.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 
 from event.factories import EventFactory
+from relation.factories import RegistrationFactory
 from user.factories import CustomUserFactory
 
 
@@ -114,6 +115,7 @@ def test_participant_cannot_create_event(get_auth_client):
     assert response.status_code == status.HTTP_403_FORBIDDEN
     assert response.data["detail"].code == "permission_denied"
 
+
 @pytest.mark.django_db
 def test_guest_cannot_create_event(api_client):
     event_url = reverse("event-list", kwargs={"version": "v1"})
@@ -124,10 +126,123 @@ def test_guest_cannot_create_event(api_client):
         "start_date": "2026-11-01T09:00:00Z",
         "title": "همایش تخصصی هوش مصنوعی و یادگیری ماشین",
         "description": "این همایش با هدف آشنایی با جدیدترین دستاوردهای حوزه هوش مصنوعی و کاربردهای آن در صنعت برگزار می‌شود. حضور برای عموم آزاد است.",
-        "status": "DRAFT"
-        }
+        "status": "DRAFT",
+    }
 
     response = api_client.post(event_url, data=event_data)
 
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
     assert response.data["detail"].code == "not_authenticated"
+
+
+@pytest.mark.django_db
+def test_event_owner_can_update_event(get_auth_client):
+    organizer = CustomUserFactory(organizer=True, password="StrongPass9!x")
+    event = EventFactory(organizer=organizer)
+
+    client = get_auth_client(organizer, password="StrongPass9!x")
+
+    event_detail_url = reverse("event-detail", kwargs={"version": "v1", "pk": event.id})
+
+    data = {"title": "همایش تخصصی هوش مصنوعی و یادگیری ماشین"}
+
+    response = client.patch(event_detail_url, data)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data["title"] == "همایش تخصصی هوش مصنوعی و یادگیری ماشین"
+
+
+@pytest.mark.django_db
+def test_organizer_cannot_update_other_organizers_event(get_auth_client):
+    organizer = CustomUserFactory(organizer=True, password="StrongPass9!x")
+    other_organizer = CustomUserFactory(organizer=True, password="StrongPass9!x")
+
+    event = EventFactory(organizer=organizer)
+
+    client = get_auth_client(other_organizer, password="StrongPass9!x")
+
+    event_detail_url = reverse("event-detail", kwargs={"version": "v1", "pk": event.id})
+
+    data = {"title": "همایش تخصصی هوش مصنوعی و یادگیری ماشین"}
+
+    response = client.patch(event_detail_url, data)
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+def test_organizer_can_publish_draft_event(get_auth_client):
+    organizer = CustomUserFactory(organizer=True, password="StrongPass9!x")
+
+    event = EventFactory(organizer=organizer)
+
+    client = get_auth_client(organizer, password="StrongPass9!x")
+
+    data = {"status": "PUBLISHED"}
+
+    event_detail_url = reverse("event-detail", kwargs={"version": "v1", "pk": event.id})
+
+    response = client.patch(event_detail_url, data)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data["status"] == "PUBLISHED"
+
+
+@pytest.mark.django_db
+def test_organizer_cannot_finish_draft_event(get_auth_client):
+    organizer = CustomUserFactory(organizer=True, password="StrongPass9!x")
+
+    event = EventFactory(organizer=organizer)
+
+    client = get_auth_client(organizer, password="StrongPass9!x")
+
+    data = {"status": "FINISHED"}
+
+    event_detail_url = reverse("event-detail", kwargs={"version": "v1", "pk": event.id})
+
+    response = client.patch(event_detail_url, data)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_organizer_cannot_finish_draft_event(get_auth_client):
+    organizer = CustomUserFactory(organizer=True, password="StrongPass9!x")
+
+    event = EventFactory(organizer=organizer)
+
+    client = get_auth_client(organizer, password="StrongPass9!x")
+
+    data = {"status": "FINISHED"}
+
+    event_detail_url = reverse("event-detail", kwargs={"version": "v1", "pk": event.id})
+
+    response = client.patch(event_detail_url, data)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_organizer_cannot_set_capacity_below_registration_count(get_auth_client):
+    organizer = CustomUserFactory(organizer=True, password="StrongPass9!x")
+
+    event = EventFactory(published=True, organizer=organizer, capacity=5)
+
+    participant1 = CustomUserFactory(participant=True)
+    participant2 = CustomUserFactory(participant=True)
+    participant3 = CustomUserFactory(participant=True)
+
+    RegistrationFactory(participant=participant1, event=event)
+    RegistrationFactory(participant=participant2, event=event)
+    RegistrationFactory(participant=participant3, event=event)
+
+
+    client = get_auth_client(organizer, password="StrongPass9!x")
+
+    event_detail_url = reverse("event-detail", kwargs={"version": "v1", "pk": event.id})
+
+    data = {"capacity": 2}
+
+    response = client.patch(event_detail_url, data)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
