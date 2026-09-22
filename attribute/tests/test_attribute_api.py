e2e4696b9f12790616f5d8b3b1ef11af8fad968f
Ehsan Barghamadi <ehsan0barghamadi@gmail.com>
2026-09-22 21:01:32 +0330

    test(attribute): add event attribute value queryset tests

--- attribute/tests/test_attribute_api.py
+++ attribute/tests/test_attribute_api.py
@@ -3,7 +3,8 @@ from django.urls import reverse
 from rest_framework import status
 
 from user.factories import CustomUserFactory
-from attribute.factories import AttributeFactory
+from attribute.factories import AttributeFactory, EventAttributeValueFactory
+from event.factories import EventFactory
 
 
 @pytest.mark.django_db
@@ -66,3 +67,88 @@ def test_staff_user_can_create_attribute(get_auth_client):
     assert response.status_code == status.HTTP_201_CREATED
     assert response.data["data_type"] == "BOOLEAN"
     assert response.data["name"] == "برگزاری جلسه"
+
+
+@pytest.mark.django_db
+def test_guest_cannot_list_event_attribute_values(api_client):
+    list_eventattributevalue_url = reverse(
+        "eventattributevalue-list", kwargs={"version": "v1"}
+    )
+    response = api_client.get(list_eventattributevalue_url)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.data["detail"].code == "not_authenticated"
+
+
+@pytest.mark.django_db
+def test_organizer_can_list_own_draft_event_attribute_values(get_auth_client):
+    organizer = CustomUserFactory(organizer=True, password="StrongPass9!x")
+    event = EventFactory(organizer=organizer)
+    event_attribute = EventAttributeValueFactory(event=event)
+
+    list_eventattributevalue_url = reverse(
+        "eventattributevalue-list", kwargs={"version": "v1"}
+    )
+
+    client = get_auth_client(organizer, password="StrongPass9!x")
+
+    response = client.get(list_eventattributevalue_url)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert any(item["id"] == event_attribute.id for item in response.data)
+    assert response.data[0]["event"] == event.id
+
+
+@pytest.mark.django_db
+def test_organizer_cannot_list_other_draft_event_attribute_values(get_auth_client):
+    organizer = CustomUserFactory(organizer=True, password="StrongPass9!x")
+    other_organizer = CustomUserFactory(organizer=True, password="StrongPass9!x")
+    event = EventFactory(organizer=other_organizer)
+    event_attribute = EventAttributeValueFactory(event=event)
+
+    list_eventattributevalue_url = reverse(
+        "eventattributevalue-list", kwargs={"version": "v1"}
+    )
+
+    client = get_auth_client(organizer, password="StrongPass9!x")
+
+    response = client.get(list_eventattributevalue_url)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert all(item["id"] != event_attribute.id for item in response.data)
+
+
+@pytest.mark.django_db
+def test_participant_can_list_published_event_attribute_values(get_auth_client):
+    participant = CustomUserFactory(participant=True, password="StrongPass9!x")
+    event = EventFactory(published=True)
+    event_attribute = EventAttributeValueFactory(event=event)
+
+    list_eventattributevalue_url = reverse(
+        "eventattributevalue-list", kwargs={"version": "v1"}
+    )
+
+    client = get_auth_client(participant, password="StrongPass9!x")
+
+    response = client.get(list_eventattributevalue_url)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert any(item["id"] == event_attribute.id for item in response.data)
+
+
+@pytest.mark.django_db
+def test_participant_cannot_list_draft_event_attribute_values(get_auth_client):
+    participant = CustomUserFactory(participant=True, password="StrongPass9!x")
+    event = EventFactory()
+    event_attribute = EventAttributeValueFactory(event=event)
+
+    list_eventattributevalue_url = reverse(
+        "eventattributevalue-list", kwargs={"version": "v1"}
+    )
+
+    client = get_auth_client(participant, password="StrongPass9!x")
+
+    response = client.get(list_eventattributevalue_url)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert all(item["id"] != event_attribute.id for item in response.data)
